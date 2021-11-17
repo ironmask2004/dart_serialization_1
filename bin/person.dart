ac0b88a0e70e0f13c5630bb00988711a041c59c1
ironmask2004 <ironmask2004@gmail.com>
2021-11-18 00:28:14 +0200

    person

--- bin/person.dart
+++ bin/person.dart
@@ -7,13 +7,12 @@ part 'person.g.dart';
 
 @JsonSerializable(explicitToJson: true)
 class Person {
-  Person(this.name, this.address, this.status);
-
   String name;
   Address address;
-
   @JsonKey(unknownEnumValue: StatusCode.none)
   StatusCode status;
+
+  Person(this.name, this.address, this.status);
 
   factory Person.fromJson(Map<String, dynamic> json) => _$PersonFromJson(json);
   Map<String, dynamic> toJson() => _$PersonToJson(this);
