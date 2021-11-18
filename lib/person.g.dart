acd3a2d3462584773c67a44ed304f6f08c1dd21f
ironmask2004 <ironmask2004@gmail.com>
2021-11-18 08:58:21 +0200

    test11

--- lib/person.g.dart
+++ lib/person.g.dart
@@ -1,19 +1,19 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'user.dart';
+part of '../lib/person.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-User _$UserFromJson(Map<String, dynamic> json) => User(
+Person _$PersonFromJson(Map<String, dynamic> json) => Person(
       json['name'] as String,
       Address.fromJson(json['address'] as Map<String, dynamic>),
       $enumDecode(_$StatusCodeEnumMap, json['status'],
           unknownValue: StatusCode.none),
     );
 
-Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+Map<String, dynamic> _$PersonToJson(Person instance) => <String, dynamic>{
       'name': instance.name,
       'address': instance.address.toJson(),
       'status': _$StatusCodeEnumMap[instance.status],
--- lib/user.g.dart
+++ lib/user.g.dart
@@ -1,19 +1,19 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'person.dart';
+part of '../lib/user.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-Person _$PersonFromJson(Map<String, dynamic> json) => Person(
+User _$UserFromJson(Map<String, dynamic> json) => User(
       json['name'] as String,
       Address.fromJson(json['address'] as Map<String, dynamic>),
       $enumDecode(_$StatusCodeEnumMap, json['status'],
           unknownValue: StatusCode.none),
     );
 
-Map<String, dynamic> _$PersonToJson(Person instance) => <String, dynamic>{
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'name': instance.name,
       'address': instance.address.toJson(),
       'status': _$StatusCodeEnumMap[instance.status],
